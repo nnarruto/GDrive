e0c3d96ce063825353610cbcc876ba8a4420068b
nnarruto <multzonedaniel@gmail.com>
2017-10-06 11:14:09 -0300

    Print command line

---
+++ src/main/java/View.java
@@ -0,0 +1,20 @@
+
+/**
+ * Utility methods to print to the command line.
+ *
+ * @author rmistry@google.com (Ravi Mistry)
+ */
+public class View {
+
+  static void header1(String name) {
+    System.out.println();
+    System.out.println("================== " + name + " ==================");
+    System.out.println();
+  }
+
+  static void header2(String name) {
+    System.out.println();
+    System.out.println("~~~~~~~~~~~~~~~~~~ " + name + " ~~~~~~~~~~~~~~~~~~");
+    System.out.println();
+  }
+}

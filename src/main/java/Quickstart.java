2b2b6f5cfdca792d62ecc23afd2dc4987c0cbb42
nnarruto <multzonedaniel@gmail.com>
2017-10-07 09:20:07 -0300

    Nova MainClass

---
+++ src/main/java/Quickstart.java
@@ -0,0 +1,158 @@
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.*;
+import com.google.api.services.drive.Drive;
+
+import com.google.api.client.http.FileContent;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.api.client.googleapis.media.MediaHttpUploader;
+import com.google.api.client.googleapis.media.MediaHttpUploaderProgressListener;
+
+class CustomProgressListener implements MediaHttpUploaderProgressListener {
+	public void progressChanged(MediaHttpUploader uploader) throws IOException {
+    switch (uploader.getUploadState()) {
+      case INITIATION_STARTED:
+        System.out.println("Initiation has started!");
+        break;
+      case INITIATION_COMPLETE:
+        System.out.println("Initiation is complete!");
+        break;
+      case MEDIA_IN_PROGRESS:
+        System.out.println(uploader.getProgress());
+        break;
+      case MEDIA_COMPLETE:
+        System.out.println("Upload is complete!");
+		}
+	}
+}
+
+public class Quickstart {
+    /** Application name. */
+    private static final String APPLICATION_NAME =
+        "Drive API Java Quickstart";
+
+    /** Directory to store user credentials for this application. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+        System.getProperty("user.home"), ".credentials/drive-java-quickstart");
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY =
+        JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
+
+    /** Global instance of the scopes required by this quickstart.
+     *
+     * If modifying these scopes, delete your previously saved credentials
+     * at ~/.credentials/drive-java-quickstart
+     */
+    private static final List<String> SCOPES =
+        Arrays.asList(DriveScopes.DRIVE);
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+    
+    public static Credential authorize() throws IOException {
+        // Load client secrets.
+        InputStream in =
+            Quickstart.class.getResourceAsStream("/client_secret.json");
+        GoogleClientSecrets clientSecrets =
+            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(DATA_STORE_FACTORY)
+                .setAccessType("offline")
+                .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+            flow, new LocalServerReceiver()).authorize("user");
+        System.out.println(
+                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+        return credential;
+    }
+
+    public static Drive getDriveService() throws IOException {
+        Credential credential = authorize();
+        return new Drive.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    public static void main(String[] args) throws IOException {
+	/*	
+        System.out.println("");
+        System.out.println("Exemplo 1....");
+        System.out.println("");
+        // Build a new authorized API client service.
+        Drive service = getDriveService();
+
+        // Print the names and IDs for up to 10 files.
+        FileList result = service.files().list()
+             .setPageSize(10)
+             .setFields("nextPageToken, files(id, name)")
+             .execute();
+        List<File> files = result.getFiles();
+        if (files == null || files.size() == 0) {
+            System.out.println("No files found.");
+        } else {
+            System.out.println("Files:");
+            for (File file : files) {
+                System.out.printf("%s (%s)\n", file.getName(), file.getId());
+            }
+        }
+	
+        System.out.println("");
+        System.out.println("Exemplo 3...");
+        System.out.println("");
+        //subir um arquivo e pegar o ID dele no Drive
+        File fileMetadata1 = new File();
+		fileMetadata1.setName("Nome_arquivo_1_noDrive.jpg");
+		java.io.File filePath1 = new java.io.File("/home/daniel/ArquivoQueVaiSubir.jpg");
+		FileContent mediaContent1 = new FileContent("image/jpeg", filePath1);
+		File file = service.files().create(fileMetadata1, mediaContent1).setFields("id").execute();
+		System.out.println("File ID: " + file.getId());
+	*/	
+        System.out.println("");
+		System.out.println("Exemplo 2....");
+        System.out.println("");		
+		//subir um arquivo usando um progress uploader personalizado
+		File fileMetadata2 = new File();
+    	fileMetadata2.setName("Nome_arquivo_2_noDrive.jpg");
+		java.io.File filePath2 = new java.io.File("/home/daniel/ArquivoQueVaiSubir.jpg");
+		FileContent mediaContent2 = new FileContent("image/jpeg", filePath2);
+		Drive.Files.Create request = service.files().create(fileMetadata2, mediaContent2);
+		request.getMediaHttpUploader().setProgressListener(new CustomProgressListener());
+		request.execute();
+    }
+
+}
